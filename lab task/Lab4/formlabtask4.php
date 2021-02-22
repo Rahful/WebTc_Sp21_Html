5231cf24fc44c5cd4106ae59ee83e006ac4e3adf
Rahful <bmshawon02@gmail.com>
2021-02-22 16:11:59 +0600

    some error found

--- lab task/Lab4/formlabtask4.php
+++ lab task/Lab4/formlabtask4.php
@@ -73,23 +73,44 @@
                 $uname=$_POST["uname"];
 
             }
+            if(empty($_POST["pass"]))
+		 {
+			 $err_pass="[Password Required]";
+		 }
+		 elseif(htmlspecialchars($_POST["pass"]))
+		 {
+			 $err_pass=["HTML KeyWords Used"];
+		 }
+		 elseif (strlen($_POST["uname"])<8) {
+		 	$err_pass=["[Password must be 8 charachters long"];
+		 }
+		 elseif(!strpos($_POST["pass"],"#"))
+		 {
+			 $err_pass="[Password should contain special character]";
+		 }
+		 elseif(!is_numeric($_POST["pass"]))
+		 {
+			 $err_pass="[Password should contain Numeric values]";
+		 }
+		 elseif(!ctype_upper($_POST["pass"]))
+		 {
+			 $err_pass="[Password should contain UpperCase values]";
+		 }
+		 elseif(!ctype_lower($_POST["pass"]))
+		 {
+			 $err_pass="[Password should contain LowerCase values]";
+		 }
 
-			if(empty($_POST["pass"])){
-                $err_pass="Enter pass";
-                
-            }
-            else if(strlen($_POST["pass"])<6){
-                $err_pass="Password must be more than 6 characters long";
-                $pass="";
-            }
-            else if(strpos($_POST["pass"]," ")){
-                $err_pass="Password should not contain whitespace";
-            }
+		 elseif(strpos($_POST["pass"]," "))
+		 {
+			 $err_pass="[Password should not contain white space]";
+		 }
+		 else
+		 {
+			 $err_pass=$_POST["pass"];
+         }
 
-            else{   
-                $pass=$_POST["pass"];
-
-            }
+            
 			if(empty($_POST["pass"])){
                 $err_cpass="Enter pass again";
                 
